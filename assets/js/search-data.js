18a37464068effe9e20dcb7abed5eebc26b095cd
qlnguyen1208 <137669813+qlnguyen1208@users.noreply.github.com>
2025-01-04 09:32:32 +0000

    Deploying to gh-pages from @ qlnguyen1208/qlnguyen1208.github.io@3a0a8bf242c59183ac28a6ae9cdba4387d1d523f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/news/";
-          },
         },{id: "nav-publication-lt-del-gt-s-lt-del-gt",
           title: "publication&lt;del&gt;s&lt;/del&gt;",
           description: "",
@@ -36,6 +29,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
           },
         },{id: "post-creating-a-pcr-primer-table-in-latex-from-a-csv-file",
       
